361ff40b9a7d623a2bc4e4972aef45a8cfe74c76
AlexandreSF123 <alexandresantosfonseca47@gmail>
2026-02-25 18:28:27 -0300

    triste

---
+++ app/Http/Controllers/Aluno.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Aluno extends Controller
+{
+    function aluno(){
+        echo 'Página do Aluno';
+    }
+    function login(){
+        echo "Login Efetuado";
+    }
+    function boletim(){
+        echo "Português: MB|R|B|I";
+    }
+    function declaração(){
+        echo "Que tipo de declaração?";
+    }
+    function(){
+        
+    }
+
+    
+}
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,17 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
+Route::get('/aluno/boletim', [App\Http\Controllers\Aluno::class, 'boletim']);
+Route::get('/professor/calendario', [App\Http\Controllers\Professor::class, 'calendario']);
+Route::get('/professor/login', [App\Http\Controllers\Professor::class, 'login']);
+Route::get('/professor/cronograma', [App\Http\Controllers\Professor::class, 'cronograma']);
+Route::get('/aluno/declaração', [App\Http\Controllers\Aluno::class, 'declaração']);
+Route::get('/aluno/matricula', [App\Http\Controllers\Aluno::class, 'matricula']);
+Route::get('/aluno/login', [App\Http\Controllers\Aluno::class, 'login']);
+Route::get('/aluno/solicitar', [App\Http\Controllers\Aluno::class, 'solicitação']);
+Route::get('/aluno/ficha_de_desempenho', [App\Http\Controllers\Aluno::class, 'fichaDesempenho']);
+Route::get('/aluno/email_institucional', [App\Http\Controllers\Aluno::class, 'emailInstituicional']);
 
 
 
