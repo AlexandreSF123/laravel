3131f06660de130788bdf752bb850ab150229d56
AlexandreSF123 <alexandresantosfonseca46@gmail.com>
2026-06-18 18:30:39 -0300

    droga

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,11 +18,33 @@ class AdminController extends Controller
                 $dados->all(),
                   [
                       'nome' => 'required|min:3|max:255',
+                      'email' => 'required|min:11|max:255',
+                      'telefone' => 'required|min:11|max:14',
+                      'cpf' => 'required|min:12|max:12',
+                      'usuario' => 'required|min:3|max:255',
+                      'senha' => 'required|min:6|max:15',
+                      'status' => 'required',
                   ],
                   [
                       'nome.required' => 'O campo nome é obrigatório.',
                       'nome.min' => 'O campo nome deve conter no mínimo 3 caracteres.',
                       'nome.max' => 'O campo nome deve conter no máximo 255 caracteres.',
+                      'email.required' => 'O campo email é obrigatório.',
+                      'email.min' => 'O campo email deve conter no mínimo 3 caracteres.',
+                      'email.max' => 'O campo email deve conter no máximo 255 caracteres.',
+                      'telefone.required' => 'O campo telefone é obrigatório.',
+                      'telefone.min' => 'O campo telefone deve conter no mínimo 3 caracteres.',
+                      'telefone.max' => 'O campo telefone deve conter no máximo 255 caracteres.',
+                      'cpf.required' => 'O campo cpf é obrigatório.',
+                      'cpf.min' => 'O campo cpf deve conter no mínimo 3 caracteres.',
+                      'cpf.max' => 'O campo cpf deve conter no máximo 255 caracteres.',
+                      'usuario.required' => 'O campo usuario é obrigatório.',
+                      'usuario.min' => 'O campo usuario deve conter no mínimo 3 caracteres.',
+                      'usuario.max' => 'O campo usuario deve conter no máximo 255 caracteres.',
+                      'senha.required' => 'O campo senha é obrigatório.',
+                      'senha.min' => 'O campo senha deve conter no mínimo 3 caracteres.',
+                      'senha.max' => 'O campo senha deve conter no máximo 255 caracteres.',
+                      'status.required' => 'O campo status é obrigatório.',
                   ]);
             $admin = new \App\Models\AdminModel();
             $admin::create($dados->all());
