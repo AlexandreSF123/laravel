28b3a60e13b845acd60f967f405c0a88d6368d65
AlexandreSF123 <alexandresantosfonseca47@gmail.com>
2026-07-29 18:19:30 -0300

    aiai

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,8 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
-Route::get('/contato', [App\Http\Controllers\Contato::class, 'contato']);
-Route::get('/aluno/boletim', [App\Http\Controllers\Aluno::class, 'boletim']);
-Route::get('/professor/calendario', [App\Http\Controllers\Professor::class, 'calendario']);
-Route::get('/professor/login', [App\Http\Controllers\Professor::class, 'login']);
-Route::get('/professor/cronograma', [App\Http\Controllers\Professor::class, 'cronograma']);
-Route::get('/aluno/declaração', [App\Http\Controllers\Aluno::class, 'declaração']);
-Route::get('/aluno/matricula', [App\Http\Controllers\Aluno::class, 'matricula']);
-Route::get('/aluno/login', [App\Http\Controllers\Aluno::class, 'login']);
-Route::get('/aluno/solicitar', [App\Http\Controllers\Aluno::class, 'solicitação']);
-Route::get('/aluno/ficha_de_desempenho', [App\Http\Controllers\Aluno::class, 'fichaDesempenho']);
-Route::get('/aluno/email_institucional', [App\Http\Controllers\Aluno::class, 'emailInstituicional']);
+Route::get('/', [App\Http\Controllers\Principal::class, 'principal'])->name('principal');
+Route::get('/sobre', [App\Http\Controllers\Principal::class, 'sobre'])->name('sobre');
+Route::get('/produtos', [App\Http\Controllers\Principal::class, 'produtos'])->name('produtos');
+Route::get('/contato', [App\Http\Controllers\Principal::class, 'contato'])->name('contato');
 
-
-Route::prefix('/aluno')->group(function(){
-    Route::get('/index', [App\Http\Controllers\AlunoController::class, 'index'])->name('aluno.index');
-    Route::post('/add', [App\Http\Controllers\AlunoController::class, 'add'])->name('aluno.add');
-    Route::post('/remove', [App\Http\Controllers\AlunoController::class, 'remove'])->name('aluno.remove');
-    Route::post('/edit', [App\Http\Controllers\AlunoController::class, 'edit'])->name('aluno.edit');
-    Route::get('/list', [App\Http\Controllers\AlunoController::class, 'list'])->name('aluno.list');
-}); 
